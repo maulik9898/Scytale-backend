368f4bbfe04c18193f91c8977fcd04ec2f60f8be
Maulik Patel <maulikp163@gmail.com>
2021-12-20 19:11:39 +0530

    Added token validate endpoint and updated response status code for invalid token

--- src/main/java/com/scytale/backend/authentication/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/scytale/backend/authentication/filter/CustomAuthorizationFilter.java
@@ -1,8 +1,5 @@
 package com.scytale.backend.authentication.filter;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scytale.backend.authentication.utility.Token;
@@ -15,7 +12,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,15 +20,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 
 import static com.scytale.backend.authentication.utility.Constants.TOKEN_ACCESS;
-import static com.scytale.backend.authentication.utility.Constants.TOKEN_REFRESH;
 import static com.scytale.backend.authentication.utility.Constants.TOKEN_TYPE;
 import static java.util.Arrays.stream;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
@@ -62,13 +56,13 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
                     UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
                     filterChain.doFilter(request, response);
-                } catch (Exception e){
-                    log.error("Error logging in : {}",e.getMessage());
-                    Map<String,String> error = new HashMap<>();
-                    error.put("error_msg",e.getMessage());
-                    response.setStatus(FORBIDDEN.value());
+                } catch (Exception e) {
+                    log.error("Error logging in : {}", e.getMessage());
+                    Map<String, String> error = new HashMap<>();
+                    error.put("error_msg", e.getMessage());
+                    response.setStatus(UNAUTHORIZED.value());
                     response.setContentType(APPLICATION_JSON_VALUE);
-                    new ObjectMapper().writeValue(response.getOutputStream(),error);
+                    new ObjectMapper().writeValue(response.getOutputStream(), error);
                 }
 
             }
